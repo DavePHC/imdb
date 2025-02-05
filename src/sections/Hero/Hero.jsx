071ab3e22be00fd5d84eda1f32f97c80fe6489a8
Дмитрий <dmitriyphc@gmail.com>
2025-02-05 22:58:30 +0300

    tech commit

---
+++ src/sections/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import "./Hero.scss"
+import Header from "@/layouts/Header/index.js";
+
+const Hero = (props) => {
+
+  return (
+    <section className="hero" aria-labelledby="hero--title">
+      <div className="hero__action">
+
+      </div>
+      <div className="hero__content"></div>
+    </section>
+  )
+}
+
+export default Hero
